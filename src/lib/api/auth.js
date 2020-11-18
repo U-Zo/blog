07462107c56c9cb4266112470bc82f0f93a8ce7d
Woojo <kwoojo0127@naver.com>
2020-11-18 14:53:05 +0900

    Update api url

--- src/lib/api/auth.js
+++ src/lib/api/auth.js
@@ -1,14 +1,16 @@
 import client from './client';
 
+const serverUrl = 'https://u-zo-server.herokuapp.com';
+
 // 로그인
-export const login = ({username, password}) =>
-    client.post('/api/auth/login', {username, password});
+export const login = ({ username, password }) =>
+  client.post(`${serverUrl}/api/auth/login`, { username, password });
 
 // 회원가입
-export const register = ({username, password}) =>
-    client.post('/api/auth/register', {username, password});
+export const register = ({ username, password }) =>
+  client.post(`${serverUrl}/api/auth/register`, { username, password });
 
 // 로그인 상태 확인
-export const check = () => client.get('/api/auth/check');
+export const check = () => client.get(`${serverUrl}/api/auth/check`);
 
-export const logout = () => client.post('/api/auth/logout');
+export const logout = () => client.post(`${serverUrl}/api/auth/logout`);
--- src/lib/api/posts.js
+++ src/lib/api/posts.js
@@ -1,27 +1,29 @@
 import qs from 'qs';
 import client from './client';
 
-export const writePost = ({title, body, tags}) =>
-    client.post('/api/posts', {title, body, tags});
+const serverUrl = 'https://u-zo-server.herokuapp.com';
 
-export const readPost = id => client.get(`/api/posts/${id}`);
+export const writePost = ({ title, body, tags }) =>
+  client.post(`${serverUrl}/api/posts`, { title, body, tags });
 
-export const listPosts = ({page, username, tag}) => {
-    const queryString = qs.stringify({
-        page,
-        username,
-        tag
-    });
+export const readPost = (id) => client.get(`${serverUrl}/api/posts/${id}`);
 
-    return client.get(`/api/posts?${queryString}`);
+export const listPosts = ({ page, username, tag }) => {
+  const queryString = qs.stringify({
+    page,
+    username,
+    tag,
+  });
+
+  return client.get(`${serverUrl}/api/posts?${queryString}`);
 };
 
-export const updatePost = ({id, title, body, tags}) => {
-    client.patch(`/api/posts/${id}`, {
-        title,
-        body,
-        tags
-    });
+export const updatePost = ({ id, title, body, tags }) => {
+  client.patch(`${serverUrl}/api/posts/${id}`, {
+    title,
+    body,
+    tags,
+  });
 };
 
-export const removePost = id => client.delete(`/api/posts/${id}`);
+export const removePost = (id) => client.delete(`${serverUrl}/api/posts/${id}`);
